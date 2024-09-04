a6ef8893ecfc38faeba5f510e762b924ccc4ec7b
1manasseh23 <136644538+1manasseh23@users.noreply.github.com>
2024-09-04 15:53:03 -0700

    UsersController

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,21 +1,70 @@
 import sha1 from 'sha1';
-import { ObjectId } from 'mongodb';
+import Queue from 'bull';
+import { findUserById, findUserIdByToken } from '../utils/helpers';
 import dbClient from '../utils/db';
 
+const userQueue = new Queue('userQueue');
+
 class UsersController {
-  static async postNew(req, res) {
-    const { email, password } = req.body;
+  /**
+   * Creates a user using email and password
+   */
+  static async postNew(request, response) {
+    const { email, password } = request.body;
 
-    if (!email) return res.status(400).json({ error: 'Missing email' });
-    if (!password) return res.status(400).json({ error: 'Missing password' });
+    // check for email and password
+    if (!email) return response.status(400).send({ error: 'Missing email' });
+    if (!password) return response.status(400).send({ error: 'Missing password' });
 
-    const userExists = await dbClient.db.collection('users').findOne({ email });
-    if (userExists) return res.status(400).json({ error: 'Already exist' });
+    // check if the email already exists in DB
+    const emailExists = await dbClient.users.findOne({ email });
+    if (emailExists) return response.status(400).send({ error: 'Already exist' });
 
-    const hashedPassword = sha1(password);
-    const result = await dbClient.db.collection('users').insertOne({ email, password: hashedPassword });
-    return res.status(201).json({ id: result.insertedId, email });
+    // Insert new user
+    const sha1Password = sha1(password);
+    let result;
+    try {
+      result = await dbClient.users.insertOne({
+        email, password: sha1Password,
+      });
+    } catch (err) {
+      await userQueue.add({});
+      return response.status(500).send({ error: 'Error creating user' });
+    }
+
+    const user = {
+      id: result.insertedId,
+      email,
+    };
+
+    await userQueue.add({
+      userId: result.insertedId.toString(),
+    });
+
+    return response.status(201).send(user);
+  }
+
+  /**
+   * Should retrieve the user base on the token used
+   */
+  static async getMe(request, response) {
+    const token = request.headers['x-token'];
+    if (!token) { return response.status(401).json({ error: 'Unauthorized' }); }
+
+    // Retrieve the user based on the token
+    const userId = await findUserIdByToken(request);
+    if (!userId) return response.status(401).send({ error: 'Unauthorized' });
+
+    const user = await findUserById(userId);
+
+    if (!user) return response.status(401).send({ error: 'Unauthorized' });
+
+    const processedUser = { id: user._id, ...user };
+    delete processedUser._id;
+    delete processedUser.password;
+    // Return the user object (email and id only)
+    return response.status(200).send(processedUser);
   }
 }
 
-export default UsersController;
+module.exports = UsersController;
